69c0cdb16de29f0fdb8b1ccadbca320534b41af9
messense <messense@icloud.com>
2023-01-27 21:15:07 +0800

    Fix typing in nh3.pyi

--- nh3.pyi
+++ nh3.pyi
@@ -4,7 +4,7 @@ def clean(
     html: str,
     tags: Optional[Set[str]] = None,
     attributes: Optional[Dict[str, Set[str]]] = None,
-    strip_comments=True,
-    link_rel="noopener noreferrer",
+    strip_comments: bool = True,
+    link_rel: Optional[str] = "noopener noreferrer",
 ) -> str: ...
 def clean_text(html: str) -> str: ...
