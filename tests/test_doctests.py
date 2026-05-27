8ec45a31335b0df9664d22749067fe41a3e369bc
gghez <6266247+gghez@users.noreply.github.com>
2026-05-27 03:16:13 +0200

    Add nh3.escape alias for clean_text (#127)
    * Add failing test for nh3.escape alias
    
    * Add nh3.escape alias for clean_text
    
    * Add escape to type stubs
    
    * Run doctests for nh3.escape

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -7,7 +7,7 @@ def test_docstrings():
     finder = doctest.DocTestFinder()
     runner = doctest.DocTestRunner(verbose=False)
     globs = {"nh3": nh3}
-    for name in ["clean", "clean_text", "is_html"]:
+    for name in ["clean", "clean_text", "escape", "is_html"]:
         obj = getattr(nh3, name)
         for test in finder.find(obj, f"nh3.{name}"):
             if test.examples:
